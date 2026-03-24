b723f9c089f99df9c4c272da78c65dfc66d66716
Benoni25 <blaybenoni10@gmail.com>
2026-03-24 08:07:01 +0000

    Logo and native splash screen fixed

--- app.config.js
+++ app.config.js
@@ -4,14 +4,12 @@ module.exports = {
     slug: "parts-plus",
     version: "1.0.0",
     orientation: "portrait",
-    icon: "./assets/images/icon.png",
+    icon: "./assets/images/logo.png",
     scheme: "partsplus",
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
     splash: {
-      image: "./assets/images/splash-icon.png",
-      resizeMode: "contain",
-      backgroundColor: "#FFD700",
+      backgroundColor: "#ffffff",
     },
     ios: {
       supportsTablet: true,
@@ -22,30 +20,29 @@ module.exports = {
       versionCode: 1,
       googleServicesFile:
         process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json",
+      enableProguardInReleaseBuilds: true,
+      enableShrinkResourcesInReleaseBuilds: true,
       adaptiveIcon: {
         backgroundColor: "#FFD700",
-        foregroundImage: "./assets/images/android-icon-foreground.png",
-        backgroundImage: "./assets/images/android-icon-background.png",
-        monochromeImage: "./assets/images/android-icon-monochrome.png",
+        foregroundImage: "./assets/images/logo.png",
+        backgroundImage: "./assets/images/logo.png",
+        monochromeImage: "./assets/images/logo.png",
       },
       edgeToEdgeEnabled: true,
       predictiveBackGestureEnabled: false,
     },
     web: {
       output: "static",
-      favicon: "./assets/images/favicon.png",
+      favicon: "./assets/images/logo.png",
     },
     plugins: [
       "expo-router",
       [
         "expo-splash-screen",
         {
-          image: "./assets/images/splash-icon.png",
-          imageWidth: 200,
-          resizeMode: "contain",
-          backgroundColor: "#FFD700",
+          backgroundColor: "#ffffff",
           dark: {
-            backgroundColor: "#000000",
+            backgroundColor: "#0a0a0a",
           },
         },
       ],
